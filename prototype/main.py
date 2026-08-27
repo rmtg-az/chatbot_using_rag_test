ec15be56c35679956d5fa0fd1f1d43fae63eb5fe
rmtg-az <aceshigh-r@ezweb.ne.jp>
2026-08-27 09:52:02 +0900

    Restore main.py

--- prototype/main.py
+++ prototype/main.py
@@ -1,8 +1,15 @@
+from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv(Path(__file__).resolve().parent.parent / ".env")
+
+
 from fastapi import FastAPI
 
 from api_router import router
 
 from logging_config import setup_logging
+
 
 setup_logging()
 
